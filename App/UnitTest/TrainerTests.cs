f411bea320afccb8207870218a91d32b80d4aba0
palm <tauman.exe@gmail.com>
2018-11-08 17:59:19 +0100

    RED test usemove

--- App/UnitTest/TrainerTests.cs
+++ App/UnitTest/TrainerTests.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using App.Model;
+using System.Collections.Generic;
 
 namespace UnitTest
 {
@@ -14,6 +15,40 @@ namespace UnitTest
             var expected = "Ash";
             var actual = trainer.Name;
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestUseMove()
+        {
+            var trainer = new Trainer("Ash");
+            var pokemon = new MockPokemon("Bulbasaur");
+            var move = new MockMove();
+            pokemon.Moves.Add(move);
+            trainer.UseMove(0);
+
+            if(trainer.TryGetMove(out var move2))
+            {
+                Assert.AreEqual(move2, move);
+            }
+            else
+            {
+                Assert.Fail("Expected a move to be locked in");
+            }
+        }
+
+
+        private class MockMove : Move
+        {
+
+        }
+
+        private class MockPokemon : Pokemon
+        {
+            new List<Move> Moves = new List<Move>();
+            public MockPokemon(string name) : base(name)
+            {
+                
+            }
         }
     }
 }
