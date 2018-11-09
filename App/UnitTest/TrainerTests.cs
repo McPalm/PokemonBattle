735b0356bd65024f2f87081752e4308b084775a0
palm <tauman.exe@gmail.com>
2018-11-09 11:41:27 +0100

    RED Trainer.Ready and Trainer.UseMove

--- App/UnitTest/TrainerTests.cs
+++ App/UnitTest/TrainerTests.cs
@@ -34,7 +34,7 @@ namespace UnitTest
             };
             mockPokemon.Setup(po => po.Moves).Returns(list);
             trainer.ActivePokemon = mockPokemon.Object;
-            if(trainer.UseMove(1))
+            if(trainer.UseMove(1) && trainer.UseMove(0))
             {
                 // expected behaviour
             }
@@ -43,6 +43,26 @@ namespace UnitTest
                 Assert.Fail("Expected a move to be locked in");
             }
         }
-        
+
+        [TestMethod]
+        public void TestReady()
+        {
+            var trainer = new Trainer();
+            if (trainer.Ready)
+                Assert.Fail("Trainer should not be ready by default.");
+            var mockPokemon = new Mock<IPokemon>();
+            var list = new List<IMove>
+            {
+                new Move(),
+            };
+            mockPokemon.Setup(po => po.Moves).Returns(list);
+            trainer.ActivePokemon = mockPokemon.Object;
+            trainer.UseMove(4);
+            if (trainer.Ready)
+                Assert.Fail("Trainer should not be Ready when picking an invalid move");
+            trainer.UseMove(0);
+            if (false == trainer.Ready)
+                Assert.Fail("Trainer should be ready after locking in a move!");
+        }
     }
 }
