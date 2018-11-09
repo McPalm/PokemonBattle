7f8400b0f64c6e1c8a2cbffa96b63c93f4252351
palm <tauman.exe@gmail.com>
2018-11-09 12:21:20 +0100

    GREEN pokemon.attributes must be a positive number

--- App/App/Model/Pokemon.cs
+++ App/App/Model/Pokemon.cs
@@ -16,12 +16,18 @@ namespace App.Model
 
         public Action<int> OnHealthChange { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public Action OnKnockout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int MaxHealth { set; get; } = 5;
-        public int Attack { set; get; } = 5;
-        public int Defence { set; get; } = 5;
-        public int SpAttack { set; get; } = 5;
-        public int SpDefence { set; get; } = 5;
-        public int Speed { set; get; } = 5;
+        public int MaxHealth { set => maxHealth = value > 0 ? value : maxHealth; get => maxHealth; }
+        private int maxHealth = 5;
+        public int Attack { set => attack = value > 0 ? value : attack; get => attack; }
+        private int attack = 5;
+        public int Defence { set => defence = value > 0 ? value : defence; get => defence; }
+        private int defence = 5;
+        public int SpAttack { set => spAttack = value > 0 ? value : spAttack; get => spAttack; }
+        private int spAttack = 5;
+        public int SpDefence { set => spDefence = value > 0 ? value : spDefence; get => spDefence; }
+        private int spDefence = 5;
+        public int Speed { set => speed = value > 0 ? value : speed; get => speed; }
+        private int speed = 5;
 
         public bool CanAct => throw new NotImplementedException();
 
